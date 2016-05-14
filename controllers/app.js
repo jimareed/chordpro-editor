6324a58901685800584760358a49d41e460e9f00
Jim Reed <ire@us-rem-ire.local>
2016-05-14 11:34:45 -0400

    Updated title and song text.

--- controllers/app.js
+++ controllers/app.js
@@ -7,7 +7,8 @@ var defaultSong = {text:
   "{t:Song Title}\n" +
   "{st:Artist Name}\n" +
   "Verse 1:\n" +
-  "Copy [Am]paste a song in [C]ChordPro [E7]format.\n"
+  "Click on the pencil and then copy [Am]paste a song in [C]ChordPro [E]format.\n"
+  "Click on [Am]a chord [C]to change it's [E7]position.\n"
 };
 
 app.service('service', function($http){
